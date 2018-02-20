c55042ea9bb50f068b448c5256ab9859dce84cd1
DUMEOU Gilles Ext OBS/OAB <PTRN4883@orange.com>
2018-02-20 14:35:50 +0100

    Ajout Models News & Users

---
+++ sql/3_add_base_news.sql
@@ -0,0 +1,36 @@
+INSERT INTO `news_type` (`nt_id`, `nt_type`)
+VALUES (NULL, 'INFOS');
+
+INSERT INTO `news_type` (`nt_id`, `nt_type`)
+VALUES (NULL, 'DOSSIERS');
+
+INSERT INTO `news_type` (`nt_id`, `nt_type`)
+VALUES (NULL, 'CLUTCH');
+
+INSERT INTO `news_type` (`nt_id`, `nt_type`)
+VALUES (NULL, 'VIDEOS');
+
+INSERT INTO `news_type` (`nt_id`, `nt_type`)
+VALUES (NULL, 'TEMPS MORT');
+
+
+INSERT INTO `news` ( `news_type`, `news_title`, `news_title_contains`, `news_img`, `news_contains`, `news_user_id`, `news_create`, `news_update`, `news_difuse`)
+VALUES ('1', 'Titre news', 'Le title contains pour préface<br/> C''est pas si mal',
+        'www.fadeaway.fr/assets/images/news_1.pgn', 'Le contains ici pour tester <br/>test <br/> <br/>test', '1',
+        '2018-02-20', '2018-02-20', '1');
+INSERT INTO `news` ( `news_type`, `news_title`, `news_title_contains`, `news_img`, `news_contains`, `news_user_id`, `news_create`, `news_update`, `news_difuse`)
+VALUES ('1', 'Titre news', 'Le title contains pour préface<br/> C''est pas si mal',
+        'www.fadeaway.fr/assets/images/news_1.pgn', 'Le contains ici pour tester <br/>test <br/> <br/>test', '1',
+        '2018-02-20', '2018-02-20', '1');
+INSERT INTO `news` ( `news_type`, `news_title`, `news_title_contains`, `news_img`, `news_contains`, `news_user_id`, `news_create`, `news_update`, `news_difuse`)
+VALUES ('1', 'Titre news', 'Le title contains pour préface<br/> C''est pas si mal',
+        'www.fadeaway.fr/assets/images/news_1.pgn', 'Le contains ici pour tester <br/>test <br/> <br/>test', '1',
+        '2018-02-20', '2018-02-20', '1');
+INSERT INTO `news` ( `news_type`, `news_title`, `news_title_contains`, `news_img`, `news_contains`, `news_user_id`, `news_create`, `news_update`, `news_difuse`)
+VALUES ('1', 'Titre news', 'Le title contains pour préface<br/> C''est pas si mal',
+        'www.fadeaway.fr/assets/images/news_1.pgn', 'Le contains ici pour tester <br/>test <br/> <br/>test', '1',
+        '2018-02-20', '2018-02-20', '1');
+INSERT INTO `news` ( `news_type`, `news_title`, `news_title_contains`, `news_img`, `news_contains`, `news_user_id`, `news_create`, `news_update`, `news_difuse`)
+VALUES ('1', 'Titre news', 'Le title contains pour préface<br/> C''est pas si mal',
+        'www.fadeaway.fr/assets/images/news_1.pgn', 'Le contains ici pour tester <br/>test <br/> <br/>test', '1',
+        '2018-02-20', '2018-02-20', '1');
