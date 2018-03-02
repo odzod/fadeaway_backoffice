a2f97f03a26f887fe3ac51c796125cb1e60c2e4b
DUMEOU Gilles Ext OBS/OAB <PTRN4883@orange.com>
2018-03-02 11:16:26 +0100

    Ajout test fb

--- public/fb.php
+++ public/fb.php
@@ -6,15 +6,13 @@
  * Time: 10:18
  */
 
-require_once __DIR__ . '/../vendor/autoload.php';
-
-use Facebook\Facebook;
+require_once __DIR__ . '/../vendor/Facebook/autoload.php';
 
 $token_page = "EAAcQrm4dKVMBAFqFoWTAv2uEmuZArGcFQRrRJvdrnEaZCZCbSrKEpjJxATr1rZC4tL3GAPSeu2MmUmTzkEPYIGOLNEdkOfLzmWz42ns7t5Nwb7PJytDAIm15tZACKGhnKkpM9EawpJnZCKxfY5DBpntcdff3biAeFdOpPDYDWLKA1n9CeBXhZCW14UmyYudxOIWCad3tx3fGQZDZD";
 $token_pubish = "EAAcQrm4dKVMBAPO9KuE19tE7g7LBUaGpe5OEMc5qKRR4279UKxuHBwo9uuzL2hi5zZCUVlOMt8ZAzqzh7boGe5k6xqp4zP3qdKsEWRPe8uVZBFueaQZAXHnesZCNhRButGWa47kPPZA6eZAj4QWr1BwOWQd6ZBCPM4f7vZC0yJlRj9kfJfqjafa74d9VsSZAQv8meAUFZCNdLkIrAZDZD";
 $token_long = "EAAcQrm4dKVMBAEZAAszzChZBUkK6gLYhTZB7B5l7FAV4hfCPIY9HF1D3jgwcD5hZCsKCfdsl2ZA96E4fd45TFbfPU7EVoaPZALJNQZAZBjgG602EgDgL81CXumJfqBFC5Qun7vCoVrZCX64CdzZCAt6cfKO6kw7DiheBenitVhEmt7rAZDZD";
 
-$fb = new Facebook([
+$fb = new \Facebook\Facebook([
     'app_id' => '1988666194733395',
     'app_secret' => '8780450b687f8dfa978d15935d69c2ce',
     'default_graph_version' => 'v2.12',
